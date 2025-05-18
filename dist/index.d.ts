661cfba2e568d73385e5be8103362bb465787a65
Kitick <kitick538@gmail.com>
2025-05-18 00:33:50 -0700

    patched root import removal

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import * as mathlib from "./math/index";
-import * as structurelib from "./structures/index";
-export declare const math: typeof mathlib;
-export declare const structures: typeof structurelib;
-export * from "./math/index";
-export * from "./structures/index";
--- src/index.ts
+++
@@ -1,8 +0,0 @@
-import * as mathlib from "./math/index";
-import * as structurelib from "./structures/index";
-
-export const math = mathlib;
-export const structures = structurelib;
-
-export * from "./math/index";
-export * from "./structures/index";
